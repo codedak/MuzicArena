99a4d44a5b5a84dfb876370040484dd902fd8196
codedak <alankrit.iitdhanbad@gmail.com>
2016-12-24 14:03:17 +0530

    Updated views.py with HTML code

--- music/views.py
+++ music/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1>This is the Music app Homepage</h1>")
+    all_albums = Album.objects.all() #This will connect to the database nd will provide all of the albums
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album ID: " + str(album_id) + "</h2>")
