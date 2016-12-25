0aa4baf43fcbad933d6642c899c414004d9a0cab
codedak <alankrit.iitdhanbad@gmail.com>
2016-12-25 15:10:37 +0530

    Template updated

--- music/views.py
+++ music/views.py
@@ -1,16 +1,13 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Album
 
 
 def index(request):
     all_albums = Album.objects.all() #This will connect to the database nd will provide all of the albums
-    template = loader.get_template('music/index.html')
     #Now we create a dictionary
-    context = {
-        'all_albums' : all_albums,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'all_albums' : all_albums}
+    return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album ID: " + str(album_id) + "</h2>")
