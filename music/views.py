02fc1543f0ac48cdbde770a90d414ae4c442f183
codedak <alankrit.iitdhanbad@gmail.com>
2016-12-26 06:28:49 +0530

    Views updated

--- music/views.py
+++ music/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Album
 
@@ -7,8 +7,15 @@ def index(request):
     all_albums = Album.objects.all() #This will connect to the database nd will provide all of the albums
     #Now we create a dictionary
     context = {'all_albums' : all_albums}
+    #return render(request, 'music/index.html', {'all_albums' : all_albums})  'OR'
     return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album ID: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("This Album Does Not Exist")
+    contxt = {'album' : album}
+    #return render(request, 'music/index.html', {'album' : album})  'OR'
+    return render(request, 'music/detail.html', contxt)
 
