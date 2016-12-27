dd8454e74145befd94f274695f8dc6025dc40ddb
codedak <alankrit.iitdhanbad@gmail.com>
2016-12-28 00:44:38 +0530

    Generic View Added

--- music/views.py
+++ music/views.py
@@ -1,25 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import Album
 
+class IndexView(request.ListView):
+    template_name = 'music/index.html'
 
-def index(request):
-    all_albums = Album.objects.all() #This will connect to the database nd will provide all of the albums
-    return render(request, 'music/index.html', {'all_albums' : all_albums})
+    def get_queryset(self):
+        return Album.objects.all()
 
-def detail(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album' : album})
-
-def favorite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        song_selected = album.song_set.get(pk=request.POST['song'])
-    except(KeyError, Song.DoesNotExist):
-        return render(request, 'music/detail,html', {
-            'album' : album,
-            'error_message' : "Invalid Song Selected",    
-        })
-    else:
-        song_selected.is_favorite = True
-        song_selected.save()
-        return render(request, 'music/detail.html', {'album': album})
+class DetailView(request.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
